2bb7d35805e708526d09da609500ea27ff1d7a83
lateeflivers <lateef.livers@orasi.com>
2014-06-10 13:31:04 -0700

    Start of chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
+    has_secure_password 
     before_save { self.email = email.downcase}
     validates :name, presence: true, length: { maximum: 50 }
-    VAILD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VAILD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+			/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true,
 	format: {with: VAILD_EMAIL_REGEX },
 	uniqueness:{case_sensitive: false}
-    has_secure_password 
     validates :password, length: { minimum: 6 }
 end
